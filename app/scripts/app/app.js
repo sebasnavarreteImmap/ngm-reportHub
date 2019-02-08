c5419edbb745743a672ebcf694d48a4a2d612163
sebasnavarreteImmap <snavarrete@immap.org>
2019-02-08 14:54:01 -0500

    bg5

--- app/scripts/app/app.js
+++ app/scripts/app/app.js
@@ -55,7 +55,7 @@ angular
 		'ngm.widget.stats',
 		'ngm.widget.table'
 	])
-	.config([ '$routeProvider', '$locationProvider', '$compileProvider', function ( $routeProvider, $compileProvider ) {
+	.config([ '$routeProvider', '$locationProvider', '$compileProvider', function ( $routeProvider, $locationPRovider, $compileProvider ) {
 
 
 
