fa139bf123549e053efe2045f367635274ecdcf5
sebasnavarreteImmap <snavarrete@immap.org>
2019-03-05 12:24:07 -0500

    changes in folder translate11

--- app/scripts/app/app.js
+++ app/scripts/app/app.js
@@ -1162,7 +1162,7 @@ angular
 
 
     $translateProvider.useStaticFilesLoader({
-        prefix: '/../../translate/locale-',
+        prefix: '../../translate/locale-',
         suffix: '.json'
       });
 
