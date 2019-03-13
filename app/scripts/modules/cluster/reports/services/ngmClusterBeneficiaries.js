969cee420a270a1a499999b845d956b5cde42cfb
sebasnavarreteImmap <snavarrete@immap.org>
2019-03-13 10:37:37 -0500

    target beneficiaries womens05 7

--- app/scripts/modules/cluster/reports/services/ngmClusterBeneficiaries.js
+++ app/scripts/modules/cluster/reports/services/ngmClusterBeneficiaries.js
@@ -62,7 +62,7 @@ angular.module( 'ngmReportHub' )
           families: 0,
           boys: 0,
           girls: 0,
-          womens05:0,
+        
           men:0,
           women:0,
           elderly_men:0,
@@ -765,7 +765,7 @@ angular.module( 'ngmReportHub' )
                   $data.families >= 0 &&
                   $data.boys >= 0 &&
                   $data.girls >= 0 &&
-                  $data.womens05 >= 0 &&
+                  
                   $data.men >= 0 &&
                   $data.women >= 0 &&
                   $data.elderly_men >= 0 &&
@@ -784,7 +784,7 @@ angular.module( 'ngmReportHub' )
                   $data.families >= 0 &&
                   $data.boys >= 0 &&
                   $data.girls >= 0 &&
-                  $data.womens05 >= 0 &&
+                 
                   $data.men >= 0 &&
                   $data.women >= 0 &&
                   $data.elderly_men >= 0 &&
@@ -825,7 +825,7 @@ angular.module( 'ngmReportHub' )
                   $data.families >= 0 &&
                   $data.boys >= 0 &&
                   $data.girls >= 0 &&
-                  $data.womens05 >= 0 &&
+                
                   $data.men >= 0 &&
                   $data.women >= 0 &&
                   $data.elderly_men >= 0 &&
